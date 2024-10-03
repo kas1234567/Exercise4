1f243a93f2bf08c35031dcc7a59b440148a6f98b
Annie <annetnatukunda31@gmail.com>
2024-10-03 17:13:28 +0300

    Create form1.vb

---
+++ form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    'create a program that uses nested loop to display multiplcation table 1-10
+    Private Sub btnMultiplication_Click(sender As Object, e As EventArgs) Handles btnMultiplication.Click
+        Dim i As Integer = 1
+        Dim j As Integer = 1
+
+        While (i <= 10)
+            j = 1
+            While (j <= 10)
+                lstBox.Items.Add(i & "X" & j & "=" & (i * j))
+                j += 1
+            End While
+            i += 1
+        End While
+    End Sub
+
+
+End Class
